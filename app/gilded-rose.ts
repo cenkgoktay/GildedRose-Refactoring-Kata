344b992d8f599e3c3ccddd7c0bb64ae318cb5e97
cenkgoktay <cenkgoktay@gmail.com>
2022-07-28 17:56:57 +0300

    Gilded Rose Requirements Edited

---
+++ app/gilded-rose.ts
@@ -0,0 +1,91 @@
+export class Item {
+  name: string;
+  sellIn: number;
+  quality: number;
+
+  constructor(name, sellIn, quality) {
+    this.name = name;
+    this.sellIn = sellIn;
+    this.quality = quality;
+  }
+}
+
+export class GildedRose {
+  items: Array<Item>;
+
+  constructor(items = [] as Array<Item>) {
+    this.items = items;
+  }
+
+  updateQuality() {
+    for (let i = 0; i < this.items.length; i++) {
+      if (this.items[i].name === "Aged Brie") {
+        if (this.items[i].quality < 50) {
+          this.items[i].quality = this.items[i].quality + 1;
+        }
+        this.items[i].sellIn = this.items[i].sellIn - 1;
+      } else if (this.items[i].name === "Sulfuras") {
+        this.items[i].quality = 80;
+      } else if (this.items[i].name === "Backstage passes") {
+        this.items[i].sellIn = this.items[i].sellIn - 1;
+        if (this.items[i].sellIn <= 0) {
+          this.items[i].quality = 0;
+        } else {
+          if (this.items[i].quality < 50) {
+            if (this.items[i].sellIn <= 5) {
+              this.items[i].quality = this.items[i].quality + 3;
+            } else if (this.items[i].sellIn <= 10) {
+              this.items[i].quality = this.items[i].quality + 2;
+            } else {
+              this.items[i].quality = this.items[i].quality + 1;
+            }
+          }
+        }
+
+        if (!(this.items[i].quality >= 50)) {
+          if (this.items[i].sellIn === 0) {
+            this.items[i].quality = 0;
+          } else if (this.items[i].sellIn <= 5) {
+            this.items[i].quality = this.items[i].quality + 3;
+          } else if (this.items[i].sellIn <= 10) {
+            this.items[i].quality = this.items[i].quality + 2;
+          } else {
+            this.items[i].quality = this.items[i].quality + 1;
+          }
+        }
+      } else if (this.items[i].name === "Conjured") {
+        this.items[i].sellIn = this.items[i].sellIn - 1;
+        if (this.items[i].sellIn <= 0) {
+          if (this.items[i].quality <= 4) {
+            this.items[i].quality = 0;
+          } else {
+            this.items[i].quality = this.items[i].quality - 4;
+          }
+        } else {
+          if (this.items[i].quality !== 0) {
+            if (this.items[i].quality === 1) {
+              this.items[i].quality = this.items[i].quality - 1;
+            } else {
+              this.items[i].quality = this.items[i].quality - 2;
+            }
+          }
+        }
+      } else {
+        this.items[i].sellIn = this.items[i].sellIn - 1;
+        if (this.items[i].quality !== 0) {
+          if (this.items[i].sellIn <= 0) {
+            if (this.items[i].quality === 1) {
+              this.items[i].quality = this.items[i].quality - 1;
+            } else {
+              this.items[i].quality = this.items[i].quality - 2;
+            }
+          } else {
+            this.items[i].quality = this.items[i].quality - 1;
+          }
+        }
+      }
+      this.items[i].name = "fixme";
+    }
+    return this.items;
+  }
+}
